ea8da56db1bebf663732f263766599608b71ab9b
Charles Perier <ch4rles.perier@gmail.com>
2023-12-02 17:35:54 +0100

    prepare next days and refactor to store different years

---
+++ 2023/day6/day5.ts
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+
+const input: string = fs.readFileSync('./input.txt', 'utf8');
+const lines: string[] = input.split('\n');
+
+for (const line of lines) {
+}
+
+console.log('Part 1:');
+console.log('Part 2:');
